50d0fd7e3bfe19801661c6d762c951a1299ea8cb
EddyGHendrickx <matthijs.hendrickx@gmail.com>
2019-12-16 09:47:02 +0100

    stuck on 1.3

--- js-complete-course/01-base/04-asv-confirm/script.js
+++ js-complete-course/01-base/04-asv-confirm/script.js
@@ -9,8 +9,13 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+function confirm() {
 
-    // your code here
 
-})();
+    let Age = document.getElementById(`Age`).value;
+
+    alert(`Your age is ${Age}`);
+
+
+
+};
