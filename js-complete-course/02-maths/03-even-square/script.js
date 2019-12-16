7b35040d473851ea0e9fc020aaeed0ca7446a8ec
EddyGHendrickx <matthijs.hendrickx@gmail.com>
2019-12-16 14:53:22 +0100

    update

--- js-complete-course/02-maths/03-even-square/script.js
+++ js-complete-course/02-maths/03-even-square/script.js
@@ -9,11 +9,13 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
 
-    document.getElementById("run").addEventListener("click", function() {
+
+document.getElementById("run").addEventListener("click", function() {
 
        for(i = 1; i < 21; i++) {
         alert(Math.sqrt(i));
-       }
-}
+        console.log(i);
+       }})
+
+    
