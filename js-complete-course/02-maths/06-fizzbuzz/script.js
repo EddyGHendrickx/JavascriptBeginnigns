b3d754f48094a51574807c14b97ff8e6dda17ca1
EddyGHendrickx <matthijs.hendrickx@gmail.com>
2019-12-17 09:14:51 +0100

    Completed 2.6 with a forloop

--- js-complete-course/02-maths/06-fizzbuzz/script.js
+++ js-complete-course/02-maths/06-fizzbuzz/script.js
@@ -12,5 +12,20 @@
 (function() {
 
     // your code here
+    for (let i = 0; i < 100; i++) {
+        if (i%5 === 0 && i%3 === 0) {
+            console.log(i + " fizzbuzz");
+        }
+        else if (i%5 === 0) {
+            console.log(i + " buzz");
+            }
+        else if (i%3 === 0) {
+            console.log(i + " fizz");
+            }
+        else {
+            console.log(i);
+            }
+        }
+// putting fizzbuzz first, else it'd run just 'fizz"
 
 })();
