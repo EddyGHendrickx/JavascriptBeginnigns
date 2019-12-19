c4b283509375283d06eccd9f6b403e6ac8363ca1
EddyGHendrickx <matthijs.hendrickx@gmail.com>
2019-12-19 14:26:57 +0100

    finished 4,5 with code of previous excersice

--- js-complete-course/05-arrays/05-walk-three/script.js
+++ js-complete-course/05-arrays/05-walk-three/script.js
@@ -39,6 +39,9 @@
         },
     ];
 
-    // your code here
-
+    document.getElementById("run").addEventListener("click", function() {
+        people.forEach(function (element) {
+            console.log(element.firstname);
+        })
+    })
 })();
