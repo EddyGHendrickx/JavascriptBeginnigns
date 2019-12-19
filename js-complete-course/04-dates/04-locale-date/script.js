bbf58ec3ccabf930fda4966a58a870b4bb63ffbf
EddyGHendrickx <matthijs.hendrickx@gmail.com>
2019-12-19 11:23:18 +0100

    Finished 4,4 with switch statements

--- js-complete-course/04-dates/04-locale-date/script.js
+++ js-complete-course/04-dates/04-locale-date/script.js
@@ -13,6 +13,77 @@
 
     // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
 
-    // your code here
+        var today = new Date();
+        var todayDay = today.getDate();
+        var todayMonth = today.getMonth();
+        var todayYear = today.getFullYear();
+        var todayHour = today.getHours();
+        var todayMinutes = today.getMinutes();
+        var DayOfTheWeek = today.getDay();
+        switch (DayOfTheWeek) {
+            case 0:
+                var todayDayOfWeek = "Sunday";
+                break;
+            case 1:
+                var todayDayOfWeek = "Monday";
+                break;
+            case 2:
+                var todayDayOfWeek = "Tuesday";
+                break;
+            case 3:
+                var todayDayOfWeek = "Wednesday";
+                break;
+            case 4:
+                var todayDayOfWeek = "Thursday";
+                break;
+            case 5:
+                var todayDayOfWeek = "Friday";
+                break;
+            case 6:
+                var todayDayOfWeek = "Saturday";
+                break;
+        }
 
+    switch (todayMonth) {
+        case 0:
+            var monthWords = "January";
+            break;
+        case 1:
+            var monthWords = "February";
+            break;
+        case 2:
+            var monthWords = "March";
+            break;
+        case 3:
+            var monthWords = "April";
+            break;
+        case 4:
+            var monthWords = "May";
+            break;
+        case 5:
+            var monthWords = "June";
+            break;
+        case 6:
+            var monthWords = "July";
+            break;
+        case 7:
+            var monthWords = "August";
+            break;
+        case 8:
+            var monthWords = "September";
+            break;
+        case 9:
+            var monthWords = "October";
+            break;
+        case 10:
+            var monthWords = "November";
+            break;
+        case 11:
+            var monthWords = "December";
+            break;
+    }
+        console.log(today.getDay());
+        console.log(todayDayOfWeek);
+        console.log(todayYear);
+    document.getElementById("target").innerHTML = `${todayDayOfWeek} ${todayDay} ${monthWords}, ${todayHour}:${todayMinutes}`;
 })();
