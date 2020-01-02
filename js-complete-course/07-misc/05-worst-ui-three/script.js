3770063a3acc87d3f8e47c0ac170a605435937b2
EddyGHendrickx <matthijs.hendrickx@gmail.com>
2020-01-02 11:29:40 +0100

    a tricky one

--- js-complete-course/07-misc/05-worst-ui-three/script.js
+++ js-complete-course/07-misc/05-worst-ui-three/script.js
@@ -11,6 +11,61 @@
 
 (function() {
 
-    // your code here
+    (function() {
+        let buttons = document.querySelectorAll("button");
+        document.getElementById("target").innerHTML = +0460000000;
+        buttons.forEach(function (button) {
+            button.addEventListener("click", function () {
+                var newNumber;
+                var targetText = +document.getElementById("target").innerHTML;
+                if (button.id === "fix-part-four"){
+                    let randomNumber = Math.floor(Math.random()*10000);
+                    let randomString = randomNumber.toString();
+                    console.log(randomString);
+                    let randomString1 = randomString.charAt(0);
+                    let randomString2 = randomString.charAt(1);
+                    document.getElementById("part-four").value = randomString1 + randomString2;
+                }
+                if (button.id === "fix-part-three"){
+                    let randomNumber = Math.floor(Math.random()*10000);
+                    let randomString = randomNumber.toString();
+                    console.log(randomString);
+                    let randomString1 = randomString.charAt(0);
+                    let randomString2 = randomString.charAt(1);
+                    document.getElementById("part-three").value = randomString1 + randomString2;
+                }
+                if (button.id === "fix-part-two"){
+                    let randomNumber = Math.floor(Math.random()*10000);
+                    let randomString = randomNumber.toString();
+                    console.log(randomString);
+                    let randomString1 = randomString.charAt(0);
+                    let randomString2 = randomString.charAt(1);
+                    document.getElementById("part-two").value = randomString1 + randomString2;
+                }
+                if (button.id === "fix-part-one"){
+                    function getRandomInt(min, max) {
+                        min = Math.ceil(min);
+                        max = Math.floor(max);
+                        return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+                    }
+                    let randomNumber = getRandomInt(460, 499);
+                    let randomString = randomNumber.toString();
+                    console.log(randomString);
+                    let randomString1 = randomString.charAt(0);
+                    let randomString2 = randomString.charAt(1);
+                    let randomString3 = randomString.charAt(2);
 
+                    document.getElementById("part-one").value = randomString1 + randomString2 + randomString3;
+                }
+
+                document.getElementById("target").innerHTML = 0 +
+                    document.getElementById("part-one").value +
+                    document.getElementById("part-two").value +
+                    document.getElementById("part-three").value +
+                    document.getElementById("part-four").value
+            });
+        })
+
+
+    })();
 })();
